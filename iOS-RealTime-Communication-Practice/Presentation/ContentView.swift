7869ece4aefd890351a0f625b56d8548fe347625
Owen <k022298@gmail.com>
2024-09-19 18:01:50 +0100

    Defined a viewModel for the contentView

--- iOS-RealTime-Communication-Practice/Presentation/ContentView.swift
+++ iOS-RealTime-Communication-Practice/Presentation/ContentView.swift
@@ -8,52 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messages = [String]()
     @State private var messageText = ""
+    @State private var viewModel: ContentViewModel
 
+    init(viewModel: ContentViewModel = DefaultContentViewModel()) {
+        self.viewModel = viewModel
+    }
     var body: some View {
         VStack {
-            List(messages, id: \.self) { message in
+            List(viewModel.messages, id: \.self) { message in
                 Text(message)
             }
             HStack {
                 TextField("Type a message...", text: $messageText)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                 Button("Send") {
-                    sendMessage()
+                    viewModel.sendMessage(messageText)
+                    messageText.removeAll()
                 }
             }
             .padding()
         }
-        .onAppear(perform: setupEventSource)
-    }
-
-    func sendMessage() {
-        let url = URL(string: "http://127.0.0.1:5000/send")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        let body: [String: String] = ["message": messageText]
-        request.httpBody = try? JSONEncoder().encode(body)
-        URLSession.shared.dataTask(with: request) { _, _, _ in
-            DispatchQueue.main.async {
-                self.messageText = ""
-            }
-        }.resume()
-    }
-
-    func setupEventSource() {
-        let url = URL(string: "http://127.0.0.1:5000/stream")!
-        var request = URLRequest(url: url)
-        request.timeoutInterval = TimeInterval.infinity
-        let task = URLSession.shared.dataTask(with: request) { data, _, _ in
-            if let data = data, let string = String(data: data, encoding: .utf8) {
-                DispatchQueue.main.async {
-                    self.messages.append(string.replacingOccurrences(of: "data: ", with: ""))
-                }
-            }
-        }
-        task.resume()
+        .onAppear(perform: viewModel.setupEventSource)
     }
 }
 
--- iOS-RealTime-Communication-Practice/Presentation/ContentViewModel.swift
+++ iOS-RealTime-Communication-Practice/Presentation/ContentViewModel.swift
@@ -5,4 +5,38 @@
 //  Created by wyn on 2024/9/19.
 //
 
-import Foundation
+import SwiftUI
+
+protocol ContentViewModel {
+    var messages: [String] { get }
+    func setupEventSource()
+    func sendMessage(_ message: String)
+}
+
+@Observable
+final class DefaultContentViewModel: ContentViewModel {
+    private(set) var messages: [String] = []
+    func setupEventSource() {
+        let url = URL(string: "http://127.0.0.1:5000/stream")!
+        var request = URLRequest(url: url)
+        request.timeoutInterval = TimeInterval.infinity
+        let task = URLSession.shared.dataTask(with: request) { data, _, _ in
+            if let data = data, let string = String(data: data, encoding: .utf8) {
+                DispatchQueue.main.async {
+                    self.messages.append(string.replacingOccurrences(of: "data: ", with: ""))
+                }
+            }
+        }
+        task.resume()
+    }
+    func sendMessage(_ message: String) {
+        let url = URL(string: "http://127.0.0.1:5000/send")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body: [String: String] = ["message": message]
+        request.httpBody = try? JSONEncoder().encode(body)
+        URLSession.shared.dataTask(with: request) { _, _, _ in
+        }.resume()
+    }
+}
