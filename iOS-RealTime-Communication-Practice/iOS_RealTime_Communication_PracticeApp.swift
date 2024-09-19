0b6d8e775ed3aba95663cde3575fccbcca146c33
Owen <k022298@gmail.com>
2024-09-19 13:57:08 +0100

    init the project

---
+++ iOS-RealTime-Communication-Practice/iOS_RealTime_Communication_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_RealTime_Communication_PracticeApp.swift
+//  iOS-RealTime-Communication-Practice
+//
+//  Created by wyn on 2024/9/19.
+//
+
+import SwiftUI
+
+@main
+struct iOS_RealTime_Communication_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
